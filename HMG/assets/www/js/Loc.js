838ce49d20ab403f129c130e7fa10bebab787809
ashady <ashady@ejada.com>
2013-05-08 12:33:29 +0300

    test web service

---
+++ HMG/assets/www/js/Loc.js
@@ -0,0 +1,23 @@
+var Loc = Loc || {};
+//
+
+// Arabic Localization Object
+var arLoc ={
+	loginPage:{
+		header:"دخول",
+		id		:"رقم الهوية",
+		mobile	:"رقم الهاتف"
+	}
+		
+}
+
+
+//English Localization Object
+var enLoc ={
+	loginPage:{
+		header:"Login",
+		id		:"ID Number",
+		mobile	:"Mobile Number"
+	}
+		
+}
